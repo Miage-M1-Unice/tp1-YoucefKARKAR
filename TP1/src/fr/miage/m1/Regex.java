e9995770b72fc9175340a2f5c5cee9c0c6fb2632
Youcef KAR <youcefkarkar.info@gmail.com>
2019-03-10 19:24:12 +0100

    ExpReg

--- TP1/src/fr/miage/m1/Regex.java
+++ TP1/src/fr/miage/m1/Regex.java
@@ -2,13 +2,23 @@ package fr.miage.m1;
 
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Regex implements FilenameFilter {
 
 	@Override
-	public boolean accept(File arg0, String arg1) {
-		// TODO Auto-generated method stub
-		return false;
+	public boolean accept(File dir, String name) {
+		File f = new File(dir,name);
+		
+		if(f.isDirectory()) {
+			return true;
+		}
+		
+		Pattern pattern = Pattern.compile("*.ino");
+		Matcher matcher = pattern.matcher(dir.getAbsolutePath());
+		
+		return matcher.matches();
 	}
 
 }
